e3b5f1ef2aa3572b61f6060da726b862a72f717b
Christian Grimberg <christian.grimberg@outlook.com>
2024-04-23 00:24:45 +0000

    :+1: Add ConsoleApp1 to 02.Clase
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

---
+++ 02.Clase/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+Supongamos que debemos hacer un control básico del stock de nuestros productos en un depósito.
+
+Nos brindan la cantidad de productos en depósito y la cantidad de productos vendidos.
+Entonces debemos preguntarnos si no existen productos en nuestro depósito, entonces debemos reponerlos.
+
+Aquí utilizamos Console.ReadLine() para obtener la cantidad a vender y seguir trabajando con variables,
+en la cual como dicha función retorna un string y la cantidad es un entero tuvimos que convertir
+utilizando la función Convert que me permite convertir entre diversos tipos de datos.
+*/
