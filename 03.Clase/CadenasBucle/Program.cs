1acde899b1c2c1ecb5baa9ee6ee4dfdf35a7b674
Christian Grimberg <christian.grimberg@outlook.com>
2024-04-24 23:58:01 +0000

    :+1: Add CadenasBucle project.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

---
+++ 03.Clase/CadenasBucle/Program.cs
@@ -0,0 +1,16 @@
+﻿string cadenaNula = null;
+string cadenaVacia = String.Empty;
+string cadenaSaludo = "Hola, soy una cadena...";
+
+Console.WriteLine(cadenaSaludo + cadenaNula + cadenaVacia);
+
+if(cadenaVacia == cadenaNula)
+{
+    Console.WriteLine("Las cadenas son iguales!");
+}
+else
+{
+    Console.WriteLine("Las cadenas no son iguales");
+}
+
+Console.ReadKey();
