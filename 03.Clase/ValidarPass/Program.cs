8ff901ccb882bd49d50f848892af5a313febe67e
Christian Grimberg <christian.grimberg@outlook.com>
2024-04-29 23:46:21 +0000

    :+1: Using iterations for validations.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

---
+++ 03.Clase/ValidarPass/Program.cs
@@ -0,0 +1,26 @@
+﻿﻿int intentos = 0;
+string contrasenia;
+bool ingreso = true;
+
+Console.Write("Ingrese la contraseña: ");
+
+
+while (ingreso)
+{
+    contrasenia = Console.ReadLine();
+    intentos++;
+    
+    if(contrasenia.Equals("usSystem-2022"))
+    {
+       //ingreso = false;
+        Console.WriteLine("Bienvenido a CoderHouse Dev");
+        break;
+    }
+    else if (intentos >= 5)
+    {
+        Console.WriteLine("Su contrase no fue valida y agoto los intentos");
+        break;
+    }
+    Console.Write("Su contrase no fue valida, intente nuevamente: ");
+
+}
