be789eac9ad42f684c30a6b943edc48c6066457e
Christian Grimberg <christian.grimberg@outlook.com>
2024-04-30 00:09:37 +0000

    :+1: First function.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

---
+++ 04.Clase/ejemplo01/Program.cs
@@ -0,0 +1,16 @@
+﻿int numero1;
+int numero2;
+
+int sumar(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+Console.Write("Ingrese el primer numero: ");
+numero1 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo numero: ");
+numero2 = int.Parse(Console.ReadLine());
+
+int total = sumar(numero1, numero2);
+
+Console.WriteLine("El total de la suma es: {0}", total);
