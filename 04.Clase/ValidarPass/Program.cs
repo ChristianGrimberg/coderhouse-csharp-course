0603165371fc09a0ae6e7740be97e2d5c74b3f41
Christian Grimberg <christian.grimberg@outlook.com>
2024-04-30 00:31:53 +0000

    :+1: Example using functions to validate passwords.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

---
+++ 04.Clase/ValidarPass/Program.cs
@@ -0,0 +1,51 @@
+﻿bool validarContraseña(string contraseña)
+{
+    return contraseña.Equals("usSystem-2022");
+}
+
+string ocultarPass(string contraseña)
+{
+    string contraseñaOculta = String.Empty;
+
+    foreach (char letter in contraseña)
+    {
+        contraseñaOculta += "*";
+    }
+
+    return contraseñaOculta;
+}
+
+void validoIngreso()
+{
+    int intentos = 0;
+    string contraseña;
+    bool ingreso = true;
+
+    Console.Write("Ingrese la contraseña: ");
+
+
+    while(ingreso)
+    {
+        contraseña = Console.ReadLine();
+        intentos++;
+        
+        if(validarContraseña(contraseña))
+        {
+            string passOculto = ocultarPass(contraseña);
+            Console.WriteLine("Bienvenido a CoderHouse Dev");
+            Console.WriteLine("Su contraseña es correcta: {0}", passOculto);
+            break;
+        }
+        else if (intentos >= 5)
+        {
+            Console.WriteLine("Su contrase no fue valida y agoto los intentos");
+            break;
+        }
+
+        int restoDeIntentos = 5- intentos;
+        Console.Write("Su contrase no fue valida, le quedan {0} intentos. Pruebe nuevamente: ", restoDeIntentos);
+
+    }
+}
+
+validoIngreso();
