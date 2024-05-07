c852216b588607a53fd669fd882a2c91a1b6a614
Christian Grimberg <christian.grimberg@outlook.com>
2024-05-07 00:52:19 +0000

    :+1: Working with classes.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

---
+++ 05.Clase/App01/Producto.cs
@@ -0,0 +1,51 @@
+﻿namespace App01;
+
+public class Producto
+{
+    // sin setter ni getters
+    private int _codigo;
+    private string _descripcion;
+    private double _precioDeCompra;
+    private double _precioDeVenta;
+    private string _categoria;
+
+    public Producto()
+    {
+        _codigo = 0;
+        _descripcion = string.Empty;
+        _precioDeCompra = 0;
+        _precioDeVenta = 0;
+        _categoria = string.Empty;
+
+        Console.WriteLine("Se inicializo el contructor por defecto");
+    }
+
+    public Producto(int codigo, string descripcion, double precioDeCompra, double precioDeVenta, string categoria)
+    {
+        this._codigo = codigo;
+        this._descripcion = descripcion;
+        this._precioDeCompra = precioDeCompra;
+        this._precioDeVenta = precioDeVenta;
+        this._categoria = categoria;
+
+        Console.WriteLine("Se inicializo el constructor sobrecargado");
+    }
+
+    public bool hayPrecioDeVenta()
+    {
+        return this._precioDeVenta > 0;
+    }
+
+    public override string ToString()
+    {
+        string retorno = string.Empty;
+
+        retorno += String.Format("El valor de {0} es {1}\n", "_codigo", _codigo);
+        retorno += String.Format("El valor de {0} es {1}\n", "_descripcion", _descripcion);
+        retorno += String.Format("El valor de {0} es {1:0.00}\n", "_precioDeCompra", _precioDeCompra);
+        retorno += String.Format("El valor de {0} es {1:0.00}\n", "_precioDeVenta", _precioDeVenta);
+        retorno += String.Format("El valor de {0} es {1}\n", "_categoria", _categoria);
+
+        return retorno;
+    }
+}
---
+++ 05.Clase/App01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace App01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto testProducto = new Producto(1, "Lapiz", 3.45f, 0, "Libreria");
+            
+            if(testProducto.hayPrecioDeVenta())
+            {
+                Console.WriteLine("{0}\n{1}","Hay precio de venta", testProducto);
+            }
+            else
+            {
+                Console.WriteLine("El producto no fue publicado aun");
+            }
+        }
+    }
+}
