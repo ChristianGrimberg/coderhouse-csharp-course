3f02f097291e6ac8c5bbb384af259f28d58cf45a
Christian Grimberg <christian.grimberg@outlook.com>
2024-05-07 01:34:59 +0000

    :+1: Continue working with classes.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

---
+++ 05.Clase/App02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace App02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario1 = new("Juan", "Bida", 28123123, "bida@gmail.com", 44, "Cabildo 2668");
+            Usuario usuario2 = new("Luis", "Lopez", 45123123, "luis@gmail.com", 17, "Piedras 8500");
+
+            Console.WriteLine("El usuario 1 es mayor de edad? {0}", usuario1.EsMayorDeEdad());
+            Console.WriteLine("El usuario 2 es mayor de edad? {0}", usuario2.EsMayorDeEdad());
+        }
+    }
+}
---
+++ 05.Clase/App02/Usuario.cs
@@ -0,0 +1,33 @@
+﻿namespace App02;
+
+public class Usuario
+{
+    private string nombre;
+    private string apellido;
+    private int dni;
+    private string mail;
+    private int edad;
+    private string domicilio;
+
+    public string Nombre { get => nombre; }
+    public string Apellido { get => apellido; }
+    public int DNI { get => dni; }
+    public string Mail { get => mail; set => mail = value; }
+    public int Edad { get => edad; }
+    public string Domicilio { get => domicilio; }
+
+    public Usuario(string _nombre, string _apellido, int _dni, string _mail, int _edad, string _dominicio)
+    {
+        this.nombre = _nombre;
+        this.apellido = _apellido;
+        this.dni = _dni;
+        this.mail = _mail;
+        this.edad = _edad;
+        this.domicilio = _dominicio;
+    }
+
+    public bool EsMayorDeEdad()
+    {
+        return this.Edad >= 18;
+    }
+}
