2f644f9626e819abd2c1b5b51a088bf3139c7838
Christian Grimberg <christian.grimberg@outlook.com>
2024-05-13 23:44:42 +0000

    :+1: Add Class 6 from the teacher.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

---
+++ 06.Clase/App1/IJugador.cs
@@ -0,0 +1,13 @@
+﻿namespace App1
+{
+    public interface IJugador
+    {
+        void correr();
+        void regatear();
+        void defender();
+        void rematar();
+        void colocarPase();
+        void soportarEmbate();
+
+    }
+}
---
+++ 06.Clase/App1/Ronaldo.cs
@@ -0,0 +1,45 @@
+﻿namespace App1
+{
+    internal class Ronaldo : Jugador , IJugador
+    {
+        public Ronaldo(int velocidad, int tiro, int regate, int defensa, int pase, int fisico) : base(velocidad, tiro, regate, defensa, pase, fisico)
+        {
+        }
+
+        public void colocarPase()
+        {
+            Console.WriteLine("Cristiano Ronaldo coloco pase con {0} de propiedad", this.Pase);
+
+        }
+
+        public void correr()
+        {
+            Console.WriteLine("Cristiano Ronaldo corrio con {0} de propiedad", this.Velocidad);
+
+        }
+
+        public void defender()
+        {
+            Console.WriteLine("Cristiano Ronaldo defendio con {0} de propiedad", this.Defensa);
+
+        }
+
+        public void regatear()
+        {
+            Console.WriteLine("Cristiano Ronaldo regateo con {0} de propiedad", this.Regate);
+
+        }
+
+        public void rematar()
+        {
+            Console.WriteLine("Cristiano Ronaldo remato con {0} de propiedad", this.Tiro);
+
+        }
+
+        public void soportarEmbate()
+        {
+            Console.WriteLine("Cristiano Ronaldo soporto el emnate con {0} de propiedad", this.Fisico);
+
+        }
+    }
+}
