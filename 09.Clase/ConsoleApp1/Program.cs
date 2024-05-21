551f80e85ac439263452a7a5fc1c04bc40b9d639
Christian Grimberg <christian.grimberg@outlook.com>
2024-05-21 01:00:39 +0000

    :computer: Example with Sql methods.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

---
+++ 09.Clase/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SqlClient;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = "Server=.; Database=SistemaGestion; User=sa; Password=P@ssw0rd;";
+
+        using(SqlConnection connection = new SqlConnection(connectionString))
+        {
+            string query = "SELECT * FROM Usuario;";
+
+            using (SqlCommand command = new SqlCommand(query,connection))
+            {
+                connection.Open();
+
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    if(dataReader.HasRows)
+                    {
+                        while(dataReader.Read())
+                        {
+                            var data = dataReader.GetString(1);
+                        }
+                    }
+                }
+            }
+
+            connection.Close();
+        }
+    }
+}
