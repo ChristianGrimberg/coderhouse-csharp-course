0d01d63dc9b64d24b1ee5ab849b614875fb6784d
Christian Grimberg <christian.grimberg@outlook.com>
2024-05-21 01:08:53 +0000

    :computer: Print every user from database.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

--- 09.Clase/ConsoleApp1/Program.cs
+++ 09.Clase/ConsoleApp1/Program.cs
@@ -8,21 +8,22 @@ class Program
     {
         string connectionString = "Server=.; Database=SistemaGestion; User=sa; Password=P@ssw0rd;";
 
-        using(SqlConnection connection = new SqlConnection(connectionString))
+        using (SqlConnection connection = new SqlConnection(connectionString))
         {
             string query = "SELECT * FROM Usuario;";
 
-            using (SqlCommand command = new SqlCommand(query,connection))
+            using (SqlCommand command = new SqlCommand(query, connection))
             {
                 connection.Open();
 
                 using (SqlDataReader dataReader = command.ExecuteReader())
                 {
-                    if(dataReader.HasRows)
+                    if (dataReader.HasRows)
                     {
-                        while(dataReader.Read())
+                        while (dataReader.Read())
                         {
                             var data = dataReader.GetString(1);
+                            Console.WriteLine(data);
                         }
                     }
                 }
